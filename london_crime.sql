61badb53b4b5c89dc66dae1ad5b283be7fe6dc84
AnastasiiaAdamchuk <86518223+AnastasiiaAdamchuk@users.noreply.github.com>
2021-06-27 14:14:27 +0200

    Update london_crime.sql

--- london_crime.sql
+++ london_crime.sql
@@ -1,7 +1,23 @@
---What categories of crimes do we have?
+--What major and minor categories of crimes do we have?
 
 SELECT 
-  DISTINCT major_category AS categories
+  major_category,
+  minor_category
 FROM `bigquery-public-data.london_crime.crime_by_lsoa` 
+GROUP BY 1, 2
+
+
+--Show total number of crimes (major and minor) by year from largest to smallest.
+
+SELECT
+  major_category,
+  minor_category,
+  year,
+  SUM(value) AS total_number_of_crimes
+FROM `bigquery-public-data.london_crime.crime_by_lsoa` 
+GROUP BY 1, 2, 3
+ORDER BY 3 DESC
+
+
 
 
