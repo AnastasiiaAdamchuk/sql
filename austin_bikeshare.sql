7b62d37cd59d115e29dc64efa153f5d6c3bc5193
AnastasiiaAdamchuk <86518223+AnastasiiaAdamchuk@users.noreply.github.com>
2021-06-30 14:11:42 +0200

    Update austin_bikeshare.sql

--- austin_bikeshare.sql
+++ austin_bikeshare.sql
@@ -17,22 +17,6 @@ GROUP BY 1, 3
 --SELECT * FROM `bigquery-public-data.new_york_citibike.citibike_stations` LIMIT 10
 --SELECT * FROM `bigquery-public-data.new_york_citibike.citibike_trips` LIMIT 10
 
-SELECT 
-  cs.name AS station_name,
-  cs.region_id,
-  COUNT(ct.start_station_id) AS total_trips,
-  SUM(ct.tripduration) AS total_trip_duration,
-  AVG(ct.tripduration) AS average_trip_duration,
-  MAX(ct.tripduration) AS maximum_trip_duration,
-  MIN(ct.tripduration) AS minimum_trip_duration,
-  AVG(cs.num_bikes_available) AS total_num_bikes_available
-FROM `bigquery-public-data.new_york_citibike.citibike_stations` cs
-LEFT JOIN `bigquery-public-data.new_york_citibike.citibike_trips` ct ON cs.station_id = ct.start_station_id
-GROUP BY 1,2
-
-
---Show station with the biggest amount of trips
-
 WITH bike_trips AS (
 SELECT 
   cs.name AS station_name,
@@ -48,10 +32,10 @@ LEFT JOIN `bigquery-public-data.new_york_citibike.citibike_trips` ct ON cs.stati
 GROUP BY 1,2
 )
 
+--Show station with the biggest amount of trips
 SELECT 
   station_name,
-  MAX(total_trips) AS maximum_total_trip
+  total_trips
 FROM bike_trips
-GROUP BY 1
 ORDER BY 2 DESC
-LIMIT 1  
+LIMIT 1 
