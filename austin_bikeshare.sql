d49250d6414925d9e9e097197400cf48738d20a0
AnastasiiaAdamchuk <86518223+AnastasiiaAdamchuk@users.noreply.github.com>
2021-06-30 13:06:27 +0200

    Update austin_bikeshare.sql

--- austin_bikeshare.sql
+++ austin_bikeshare.sql
@@ -16,3 +16,14 @@ GROUP BY 1, 3
 --columns to use from ct table: ct.tripduration, COUNT(ct.start_station_id)
 --SELECT * FROM `bigquery-public-data.new_york_citibike.citibike_stations` LIMIT 10
 --SELECT * FROM `bigquery-public-data.new_york_citibike.citibike_trips` LIMIT 10
+SELECT 
+  cs.name AS station_name,
+  cs.region_id,
+  ct.bikeid,
+  COUNT(ct.start_station_id) AS total_trips,
+  SUM(ct.tripduration) AS total_trip_duration,
+  AVG(ct.tripduration) AS average_trip_duration,
+  MAX(ct.tripduration) AS maximum_trip_duration,
+  MIN(ct.tripduration) AS minimum_trip_duration,
+  SUM(cs.num_bikes_available) AS total_num_bikes_available,
+FROM `bigquery-public-data.new_york_citibike.citibike_stations`
