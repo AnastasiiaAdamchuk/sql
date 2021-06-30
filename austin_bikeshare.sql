e75eee8895ecc633175226512e5a56e0a1b212fb
AnastasiiaAdamchuk <86518223+AnastasiiaAdamchuk@users.noreply.github.com>
2021-06-30 12:52:27 +0200

    Update austin_bikeshare.sql

--- austin_bikeshare.sql
+++ austin_bikeshare.sql
@@ -7,3 +7,12 @@ FROM  `bigquery-public-data.austin_bikeshare.bikeshare_stations` bs
 RIGHT JOIN `bigquery-public-data.austin_bikeshare.bikeshare_trips` bt ON bs.station_id = bt.start_station_id
 WHERE start_station_name = 'Pease Park'
 GROUP BY 1, 3
+
+
+
+--operations: total, avg, max, min, sum, per each station
+--join columns: cs.station_id, cs.region_id, ct.start_station_id, ct.bikeid
+--columns to use from cs table: cs.name, cs.region_id, SUM(cs.num_bikes_available)
+--columns to use from ct table: ct.tripduration, COUNT(ct.start_station_id)
+--SELECT * FROM `bigquery-public-data.new_york_citibike.citibike_stations` LIMIT 10
+--SELECT * FROM `bigquery-public-data.new_york_citibike.citibike_trips` LIMIT 10
